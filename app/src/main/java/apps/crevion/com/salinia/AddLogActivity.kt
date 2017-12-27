0629a2014e969e21880ae06f1f785b93c86abd66
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-12-27 22:04:23 +0700

    Update layout add log

--- app/src/main/java/apps/crevion/com/salinia/AddLogActivity.kt
+++ app/src/main/java/apps/crevion/com/salinia/AddLogActivity.kt
@@ -12,11 +12,6 @@ class AddLogActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_log)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
--- app/src/main/java/apps/crevion/com/salinia/LogActivity.kt
+++ app/src/main/java/apps/crevion/com/salinia/LogActivity.kt
@@ -2,7 +2,6 @@ package apps.crevion.com.salinia
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
