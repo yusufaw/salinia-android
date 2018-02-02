49195bb4f46535c0afb809db9798ac4479ac5d9e
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-02-02 14:24:47 +0700

    Add user model

---
+++ app/src/main/java/apps/crevion/com/salinia/model/User.kt
@@ -0,0 +1,7 @@
+package apps.crevion.com.salinia.model
+
+/**
+ * Created by yusufaw on 2/2/18.
+ */
+
+class User(val id: String, val firstName: String, val lastName: String, val email: String, val photoProfile: String)
