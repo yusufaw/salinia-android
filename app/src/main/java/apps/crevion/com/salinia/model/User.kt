167388de84b8e4d0716b7b3a4e6d0f147844e4a5
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-02-08 21:41:12 +0700

    Add SerializeName on user model

--- app/src/main/java/apps/crevion/com/salinia/model/User.kt
+++ app/src/main/java/apps/crevion/com/salinia/model/User.kt
@@ -1,7 +1,14 @@
 package apps.crevion.com.salinia.model
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by yusufaw on 2/2/18.
  */
 
-class User(val id: String, val firstName: String, val lastName: String, val email: String, val photoProfile: String)
+class User(
+        val id: String,
+        @SerializedName("first_name") val firstName: String,
+        @SerializedName("last_name") val lastName: String,
+        val email: String,
+        val photoProfile: String)
